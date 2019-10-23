9fda2f2a436cf868a384b1fd00a788c95edae9b3
Jingkang Zhao <jz132@duke.edu>
2019-10-23 12:27:43 -0400

    start to build trinucleotide background model

---
+++ ssm-background/trinucleotide_bg_enhancer.R
@@ -0,0 +1,65 @@
+library(tidyverse)
+library(fuzzyjoin)
+
+# file paths and file names
+icgc.data.path <- "/Users/jz132/Desktop/Gordanlab/Data/ICGC"
+genomic.interval.path <- "/Users/jz132/r_projects/cancer-mutations/pelinks"
+filename <- "simple_somatic_mutation.open.LIAD-FR.tsv"
+
+keep_cols <- c("icgc_mutation_id", 
+               "icgc_donor_id", 
+               "chromosome",
+               "chromosome_start",
+               "chromosome_end",
+               "assembly_version",
+               "mutation_type",
+               "reference_genome_allele",
+               "mutated_from_allele",
+               "mutated_to_allele",
+               "consequence_type",
+               "gene_affected",
+               "transcript_affected",
+               "sequencing_strategy")
+
+# import ICGC data
+setwd(icgc.data.path)
+data_icgc_try <- read_tsv(filename, col_names = T, n_max = 5)
+col_indicator <- paste(ifelse(colnames(data_icgc_try) %in% keep_cols, "?", "-"), collapse = "")
+data_icgc_raw <- read_tsv(filename, col_names = T, col_types = col_indicator) %>%
+  mutate(chromosome = paste0("chr", chromosome))
+
+all_types <- data_icgc_raw %>% distinct(consequence_type) %>% pull()
+nonexome <- c("intron_variant", "intragenic_variant", "upstream_gene_variant", 
+              "downstream_gene_variant", "intergenic_region")
+exome <- dplyr::setdiff(all_types, c(nonexome, NA))
+
+# focus on single base substitution in WGS only
+data_icgc_wgs <- data_icgc_raw %>% 
+  filter(sequencing_strategy == "WGS", mutation_type == "single base substitution")
+num_donors <- length(data_icgc_wgs %>% distinct(icgc_donor_id) %>% pull())
+num_mutations <- length(data_icgc_wgs %>% distinct(icgc_mutation_id, icgc_donor_id) %>% pull())
+
+# import genomic coordinates of promoters, enhancers, and exons
+setwd(genomic.interval.path)
+data_enhancers_fantom <- read_delim("all_enhancers_fantom.bed", delim = "\t",
+                                    col_names = c("chromosome", "start", "end"))
+
+# check how fantom annotates the mutations
+data_icgc_wgs_to_join <- data_icgc_wgs %>%
+  filter(!is.na(consequence_type)) %>% 
+  select(chromosome = chromosome,
+         start = chromosome_start,
+         end = chromosome_end,
+         icgc_mutation_id,
+         icgc_donor_id) %>%
+  distinct()
+
+mut_enhancer <- data_enhancers_fantom %>% 
+  genome_left_join(data_icgc_wgs_to_join) %>%
+  na.omit() %>%
+  select(chromosome = chromosome.y,
+         start = start.y,
+         end = end.y,
+         icgc_mutation_id,
+         icgc_donor_id) %>%
+  distinct()
