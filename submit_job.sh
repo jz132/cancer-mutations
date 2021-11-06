5e6ab4eb5e444e348f0ea10068af7890a1dcfa26
Vincentius Martin <vincentius.martin@duke.edu>
2021-11-06 12:21:43 -0400

    add dataset

--- submit_job.sh
+++ submit_job.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH -o slurmlog/vm_%A_%a.out
-#SBATCH -e slurmlog/vm_%A_%a.err
+#SBATCH -o slurmlog/job_%A_%a.out
+#SBATCH -e slurmlog/job_%A_%a.err
 
 #SBATCH --array=1-667%67
 #SBATCH --mem=2G
 #SBATCH --mail-type=END
-#SBATCH --mail-user=vm76@duke.edu
+#SBATCH --mail-user=<mail_here>
 
 Rscript muteffect_entropy.R $SLURM_ARRAY_TASK_ID
